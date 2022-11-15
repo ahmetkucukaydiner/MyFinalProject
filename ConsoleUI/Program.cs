c84f1cab027701bea3010a69bac7382ee7b12596
Ahmet Küçükaydıner <ahmetkucukaydiner@gmail.com>
2022-11-15 22:46:14 +0300

    15.11.2022 Ders sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,6 +10,25 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //Data Transformation Object(DTO)
+            ProductTest();
+            //IoC container öğrenilecek
+            //CategoryTest();
+
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
             //foreach (var product in productManager.GetAll())
@@ -22,11 +41,18 @@ namespace ConsoleUI
                 Console.WriteLine(product.ProductName);
             }
 
-            Console.WriteLine("***********************************************");
+            Console.WriteLine("************GetByUnitPrice*********");
 
             foreach (var product in productManager.GetByUnitPrice(40, 100))
             {
                 Console.WriteLine(product.ProductName);
+            }
+
+            Console.WriteLine("************Product DTO*************");
+
+            foreach (var product in productManager.GetProductDetails())
+            {
+                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
             }
         }
     }
