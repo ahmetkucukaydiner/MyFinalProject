b90ca37ea8e585adec7ad4563d23334de5ce174d
Ahmet Küçükaydıner <ahmetkucukaydiner@gmail.com>
2022-11-28 23:53:55 +0300

    28.11.2022 Ders sonu. Business motoru eklendi.

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -6,5 +6,8 @@
         public static string ProductNameInvalid = "Ürün ismi geçersiz.";
         public static string MaintenanceTime = "Sistem bakımda!";
         public static string ProductsListed = "Ürünler listelendi.";
+        public static string ProductCountOfCategoryError = "Bir kategoride maksimum 10 ürün yer alabilir";
+        public static string ProductNameAlreadyExists = "Bu isimde zaten başka bir ürün var.";
+        public static string CategoryLimitExceded = "Kategori limiti aşıldığı için yeni ürün eklenemiyor.";
     }
 }
--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -16,6 +16,9 @@ namespace Business.DependencyResolvers.Autofac
             builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance(); // biri senden IproductService isterse Product Manager ver demek.
             builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
 
+            builder.RegisterType<CategoryManager>().As<ICategoryService>().SingleInstance();
+            builder.RegisterType<EfCategoryDal>().As<ICategoryDal>().SingleInstance();
+
             var assembly = System.Reflection.Assembly.GetExecutingAssembly();
 
             builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
